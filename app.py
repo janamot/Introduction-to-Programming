fdbe142370e1f3163cf32cd2beaeb329182fbec2
janamot <mottlova.jana06@gmail.com>
2026-09-23 22:35:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
 jmeno = input("Jak se jmenujete?")
-while True:
-    style = input("Formální [F] nebo neformální [N] pozdrav?")
-    if style == "F" or style == "f":
-        print("Dobrý den " + jmeno)
-        break
-    elif style == "N" or style == "n":
-        print("Brý jitro " + jmeno)
-        break
-    else:
-        print("Neplatný vstup")
+print("Brý jitro " + jmeno)
+break
